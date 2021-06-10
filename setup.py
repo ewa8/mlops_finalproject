8272fbd57cb3882a56555cbe2418ba0c6bb580cd
Ewa Rusiecka <ewarusiecka@Ewas-MacBook.local>
2021-06-10 10:08:57 +0200

    adding cookiecutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repo for the final project for MLOps course at DTU',
+    author='Ewa',
+    license='MIT',
+)

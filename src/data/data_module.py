0d29abec67ab95bed9555e3a0a460a3d65ba262b
StefPetro <43845091+StefPetro@users.noreply.github.com>
2021-06-16 18:19:08 +0200

    Modifications to the data_module

--- src/data/data_module.py
+++ src/data/data_module.py
@@ -1,23 +1,24 @@
-"""
-Credit to: https://www.kaggle.com/pankajj/fashion-mnist-with-pytorch-93-accuracy
-"""
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, TensorDataset
 import pytorch_lightning as pl
 
 class DataModule(pl.LightningDataModule):
 
     def __init__(self, train_data_dir: str = '', test_data_dir: str = '', batch_size: int = 100):
         super().__init__()
-        self.train_data = torch.load(train_data_dir)
-        self.test_data = torch.load(test_data_dir)
-
+        self.train_data = torch.load(f'{train_data_dir}/train.pt')
+        self.test_data = torch.load(f'{train_data_dir}/test.pt')
+        
+        self.train_dataset = TensorDataset(self.train_data['data'], self.train_data['target'])
+        self.test_dataset = TensorDataset(self.train_data['data'], self.train_data['target'])
+        
+        self.batch_size = batch_size
 
     def setup(self, stage = None):
         pass
 
     def train_dataloader(self):
-        return DataLoader(self.train_data, batch_size=self.batch_size)
+        return DataLoader(self.train_dataset, batch_size=self.batch_size)
 
     def test_dataloader(self):
         return DataLoader(self.test_data, batch_size=self.batch_size)

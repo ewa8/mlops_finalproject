548e79dd25a0689499b6bfae2878d8d39cc7b205
selmameyer <selma.meyer@gmail.com>
2021-06-23 11:46:13 +0200

    test

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,11 +2,11 @@ import src.data.make_dataset as md
 
 def test_divideFilename():
 
-    filename = 'yes 56.jpg'
+    filename = 'yes56.jpg'
     
     f, e = md.findFileExtension(filename)
 
-    assert f =='yes 56'
+    assert f =='yes56'
     assert e =='jpg'
     
 

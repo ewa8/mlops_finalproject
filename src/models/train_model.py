01972e99567f06ea461e7f36f39264e4dbd1932b
Ewa Rusiecka <ewarusiecka@Ewas-MacBook.local>
2021-06-23 15:23:14 +0200

    checking make dataset

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -8,46 +8,47 @@ from src.data.data_module import DataModule
 
 import argparse
 import sys
+import os
 
 from src.models.model import TumorClassifier
 
 def train():
     os.system("python ../data/make_dataset.py --init")
     os.system("python ../data/make_dataset.py")
-    print('Training model')
-    parser = argparse.ArgumentParser(description='Training arguments')
-    parser.add_argument('--use_wandb', default=False)
-    # add any additional argument that you want
-    args = parser.parse_args(sys.argv[1:])
-    print(args)
+    # print('Training model')
+    # parser = argparse.ArgumentParser(description='Training arguments')
+    # parser.add_argument('--use_wandb', default=False)
+    # # add any additional argument that you want
+    # args = parser.parse_args(sys.argv[1:])
+    # print(args)
 
-    run =  Run.get_context()
-    mlflow_url = run.experiment.workspace.get_mlflow_tracking_uri()
+    # run =  Run.get_context()
+    # mlflow_url = run.experiment.workspace.get_mlflow_tracking_uri()
 
-    mlf_logger = MLFlowLogger(experiment_name=run.experiment.name, tracking_uri=mlflow_url)
-    mlf_logger._run_id = run.id
+    # mlf_logger = MLFlowLogger(experiment_name=run.experiment.name, tracking_uri=mlflow_url)
+    # mlf_logger._run_id = run.id
 
-    if args.use_wandb:  # Use wandb logger
-        print('Using wandb...')
-        kwargs = {'entity': 'sems'}
-        chosen_logger = WandbLogger(project='final_project', **kwargs)
-    else:  # Use default logger
-        chosen_logger = True
+    # if args.use_wandb:  # Use wandb logger
+    #     print('Using wandb...')
+    #     kwargs = {'entity': 'sems'}
+    #     chosen_logger = WandbLogger(project='final_project', **kwargs)
+    # else:  # Use default logger
+    #     chosen_logger = True
     
-    model = TumorClassifier()
-    data = DataModule(data_dir='../../brain_tumor_dataset/processed', batch_size=24)
+    # model = TumorClassifier()
+    # data = DataModule(data_dir='../../brain_tumor_dataset/processed', batch_size=24)
     
-    trainer = pl.Trainer(
-        logger=chosen_logger, 
-        max_epochs=10, 
-        log_every_n_steps=2,
-        gpus=1 if torch.cuda.is_available() else 0,
-        )
-    trainer.fit(model, datamodule=data)
+    # trainer = pl.Trainer(
+    #     logger=chosen_logger, 
+    #     max_epochs=10, 
+    #     log_every_n_steps=2,
+    #     gpus=1 if torch.cuda.is_available() else 0,
+    #     )
+    # trainer.fit(model, datamodule=data)
     
 
-    script_model = torch.jit.script(model)
-    script_model.save('../../models/deployable_model.pt')
+    # script_model = torch.jit.script(model)
+    # script_model.save('../../models/deployable_model.pt')
 
 # def evaluate():
 #     print("Evaluating until hitting the ceiling")

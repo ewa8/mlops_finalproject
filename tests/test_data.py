d92989ba1a52dbf76da126e9b06293aa07362271
selmameyer <selma.meyer@gmail.com>
2021-06-23 11:26:55 +0200

    test try

---
+++ tests/test_data.py
@@ -0,0 +1,11 @@
+import scr.data.make_dataset as md
+
+def test_divideFilename():
+
+    filename = 'yes 56.jpg'
+    
+    f, e = md.findFileExtension(filename)
+
+    assert f =='yes 56'
+    assert e =='jpg'
+    
